49e3fd70e101652f1f9a5294e4bef7cd9957e59e
tpmdjd123456 <baziz638@yahoo.com>
2018-09-02 15:44:06 +0200

    game

---
+++ New Unity Project (8)/Assets/scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class MenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // "Stage1" is the name of the first scene we created.
+        Application.LoadLevel("Stage1");
+    }
+}
+
